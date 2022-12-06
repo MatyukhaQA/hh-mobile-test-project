4fa12b4c67fb9b4d82523447ba280dabab5f0c65
matyukha <m@matyuha.ru>
2022-12-06 22:38:11 +0300

    added logo, fixed mistakes

--- test/test_hh.py
+++ test/test_hh.py
@@ -15,16 +15,17 @@ PASSWORD = os.getenv('PASSWORD')
 @allure.tag('mobile')
 def test_job_search():
     with allure.step('Поиск вакансии'):
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/fragment_intentions_onboarding_choose_direction_image_close')) \
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/fragment_intentions_onboarding_choose_direction_image_close'))\
             .click()
         browser.element((AppiumBy.ID, 'ru.hh.android:id/view_main_search_text_view_position')).click()
         browser.element((AppiumBy.ID, 'ru.hh.android:id/toolbar_search_query')).send_keys('QA automation')
         browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_compound_left_image_title_text_view_title')).click()
     with allure.step('Проверяем страницу вакансии'):
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_button_response')) \
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_button_response'))\
             .should(have.text('Откликнуться'))
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_text_view_job_position')).should(have.text('QA'))
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_text_view_job_position')) \
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_text_view_job_position'))\
+            .should(have.text('QA'))
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_vacancy_card_text_view_job_position'))\
             .should(have.text('Automation'))
 
 
@@ -58,8 +59,9 @@ def test_information_about_app():
         browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="O приложении"]')).click()
     with allure.step('Проверяем страницу'):
         browser.element((AppiumBy.CLASS_NAME, 'android.widget.ScrollView')).should(be.visible)
-        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Версия: 7.2"]')).should(have.text('Версия: 7.2'))
-        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Оценить приложение"]')) \
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Версия: 7.2"]'))\
+            .should(have.text('Версия: 7.2'))
+        browser.element((AppiumBy.XPATH, '//android.widget.TextView[@text="Оценить приложение"]'))\
             .should(have.text('Оценить приложение'))
 
 
@@ -70,7 +72,7 @@ def test_main_page():
             .click()
     with allure.step('Проверяем главную страницу'):
         browser.element((AppiumBy.CLASS_NAME, 'android.view.ViewGroup')).should(be.visible)
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_section_header_large_narrow_text_view')) \
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/cell_section_header_large_narrow_text_view'))\
             .should(have.text('Вакансии для вас'))
         assert len(browser.elements((AppiumBy.ID, 'ru.hh.android:id/bottom_navigation_container'))) == 5
 
@@ -85,5 +87,5 @@ def test_view_vacancy():
         browser.element((AppiumBy.ID, 'ru.hh.android:id/menu_vacancy_info_hidden_add')).should(be.visible)
         browser.element((AppiumBy.ID, 'ru.hh.android:id/menu_vacancy_info_share')).should(be.visible)
         browser.element((AppiumBy.ID, 'ru.hh.android:id/menu_vacancy_info_favorite_add')).should(be.visible)
-        browser.element((AppiumBy.ID, 'ru.hh.android:id/view_hh_button_title_subtitle_text_view_title')) \
+        browser.element((AppiumBy.ID, 'ru.hh.android:id/view_hh_button_title_subtitle_text_view_title'))\
             .should(have.text('Откликнуться'))
